e072a380cf56226b639bb4b3db422127f45d6d5f
Annath Beevi <annathbeevi01@gmail.com>
2023-12-01 09:20:35 +0530

    Todo complete

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -10,16 +10,14 @@ function App() {
     completed: false
   });
 
-  const styles = { textDecorationLine: formData.completed ? "line-through" : "none" };
-
-  useEffect(()=>{
-      getTask()
-  },[])
-
   const getTask = async () => {
     await axios.get("http://localhost:5000/api/tasks")
     .then((fetched) => setTask(fetched.data.task))
   }
+
+  useEffect(()=>{
+    getTask()
+  },[])
 
   const addTask=(e)=>{
     const {value, name} = e.target;
@@ -33,6 +31,7 @@ function App() {
   };
 
   const handleSubmit = async (e) => {
+    e.preventDefault()
     try {
       if(task.length !== 0){
         await axios.post('http://localhost:5000/api/tasks', formData);
@@ -61,13 +60,11 @@ function App() {
     getTask()
   };
 
-  const handleCheckbox =async () => {
-    setFormData({ completed: !formData.completed })
-    try {
-      await axios.post('http://localhost:5000/api/tasks', formData);
-    } catch (error) {
-      console.log(error);
-    }
+  const handleCheckbox =  (index) => {
+    const checkTask = task.map((task, i) => 
+      i === index ? { ...task, completed: !task.completed} : task  
+    )
+    setTask(checkTask)
   };
 
   return (
@@ -79,10 +76,10 @@ function App() {
       <button className='button' onClick={handleSubmit}>Add Task</button>
       </div>
       <ul>
-        {task.map((data)=>
-        <li className='li' key={data._id}> 
-        <input type="checkbox" onChange={handleCheckbox} value={formData.completed} /> 
-        <span style={styles}>{data.name}</span>
+        {task.map((data, index)=>
+        <li className='li' key={index}> 
+        <input type="checkbox" checked={data.completed} onChange={() => handleCheckbox(index)} /> 
+        <span className={data.completed ? "completed" : ""}>{data.name}</span>
         <button className='button1' onClick={()=>deletetask(data._id)}>Delete</button>
         <button className='button1' onClick={()=>updateTask(data._id)}>Update</button>
         </li> 
